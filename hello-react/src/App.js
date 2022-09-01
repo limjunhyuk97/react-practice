50253ed621ba6f97499079c6bfb594f4992e95b3
jun_lim97 <limjunhyuk97@gmail.com>
2022-09-01 21:35:29 +0900

    리액트 연습을 위한 리포지토리 생성

---
+++ hello-react/src/App.js
@@ -0,0 +1,16 @@
+import { FuncitonComponent, ClassComponent } from "./Practice01/Components";
+
+const App = () => {
+  return (
+    <>
+      <FuncitonComponent>
+        {" "}
+        <div>hello</div>
+        <div>why</div>{" "}
+      </FuncitonComponent>
+      <ClassComponent></ClassComponent>
+    </>
+  );
+};
+
+export default App;
