7daa47a25f8c52d9baecde151c3b5a19c8248c1d
jun_lim97 <limjunhyuk97@gmail.com>
2023-02-23 23:14:37 +0900

    [ReactJS로 영화 웹 서비스 만들기] usememo : 부모 컴포넌트의 변경된 state를 참조하는 자식 컴포넌트만 변경되도록 강제 / useState : 컴포넌트 state와 state 변경 위한 함수 제공 / useEffect : 함수형 컴포넌트 생성 및 랜더링 시 + 의존하는 state의 변경이 일어날 경우 마다 첫번째 인자로 전달한 함수 실행

--- Nomad01/demo01/src/App.js
+++ Nomad01/demo01/src/App.js
@@ -6,19 +6,31 @@ import { useState, useEffect } from "react";
 // state 변경마다 함수형 컴포넌트를 다시 실행시키지 않기 위해서는 useEffect를 사용해야 한다. (api를 호출하거나 할 때)
 function App() {
   const [number, setNumber] = useState(0);
+  const [keyword, setKeyword] = useState("");
 
   console.log("i run all the time");
 
+  // 컴포넌트가 랜더링된 초기에만 실행된다.
   useEffect(() => {
     console.log("I run only once");
   }, []);
+
+  // keyword라는 state가 변경될때만 실행된다.
+  useEffect(() => {
+    console.log("Keyword ", keyword, " find");
+  }, [keyword]);
 
   const onClick = () => {
     setNumber((number) => number + 1);
   };
 
+  const onChange = (event) => {
+    setKeyword((keyword) => event.target.value);
+  };
+
   return (
     <div>
+      <input type="text" placeholder="Search here" onChange={onChange} />
       <span>{number}</span>
       <button onClick={onClick}>Click me</button>
     </div>
