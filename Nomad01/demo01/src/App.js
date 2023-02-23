7814797ad064a0ddaa579dfa727507140e05f28d
jun_lim97 <limjunhyuk97@gmail.com>
2023-02-23 23:07:56 +0900

    [ReactJS로 영화 웹 서비스 만들기] useEffect는 component가 처음 랜더링 되었을 때 특정 코드가 한번만 실행되게끔 도와주는 hook이다.

--- Nomad01/demo01/src/App.js
+++ Nomad01/demo01/src/App.js
@@ -1,21 +1,26 @@
 import Button from "./Button";
 import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
+// state 변경 -> component는 다시 생성된다 -> 함수형으로 생성한 컴포넌트는 함수 내부 코드가 다시 동작한다
+// state 변경마다 함수형 컴포넌트를 다시 실행시키지 않기 위해서는 useEffect를 사용해야 한다. (api를 호출하거나 할 때)
 function App() {
+  const [number, setNumber] = useState(0);
+
+  console.log("i run all the time");
+
+  useEffect(() => {
+    console.log("I run only once");
+  }, []);
+
+  const onClick = () => {
+    setNumber((number) => number + 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p className={styles.title}>shit</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Button text="shit" />
-      </header>
+    <div>
+      <span>{number}</span>
+      <button onClick={onClick}>Click me</button>
     </div>
   );
 }
