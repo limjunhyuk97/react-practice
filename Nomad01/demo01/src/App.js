4e89ed393012db51ab9f1eb66f63c3ac650db71f
jun_lim97 <limjunhyuk97@gmail.com>
2023-02-23 23:57:01 +0900

    [ReactJS로 영화 웹 서비스 만들기] useEffect : 컴포넌트가 마운트될 때 특정 코드 실행 + state 변경에 따라 특정 코드 실행 + return 하는 함수를 통해 컴포넌트가 언마운트 될 경우 특정 코드 실행(clean-up / destroy) + useEffect는 특정한 랜더링에 속하게 된다

--- Nomad01/demo01/src/App.js
+++ Nomad01/demo01/src/App.js
@@ -2,42 +2,33 @@ import Button from "./Button";
 import styles from "./App.module.css";
 import { useState, useEffect } from "react";
 
-// state 변경 -> component는 다시 생성된다 -> 함수형으로 생성한 컴포넌트는 함수 내부 코드가 다시 동작한다
-// state 변경마다 함수형 컴포넌트를 다시 실행시키지 않기 위해서는 useEffect를 사용해야 한다. (api를 호출하거나 할 때)
 function App() {
-  const [number, setNumber] = useState(0);
-  const [keyword, setKeyword] = useState("");
+  const [showing, setShowing] = useState(true);
 
-  console.log("i run all the time");
+  const Hello = () => {
+    // 컴포넌트가 랜더링된 뒤 어떤 일을 수행해야 하는지 알려준다. 즉, DOM 업데이트 수행 이후 전달한 함수를 실행시킨다.
+    // 각각의 effect는 특정한 랜더링에 속하게 된다.
+    useEffect(() => {
+      console.log("I'm Here! Created!");
 
-  // 컴포넌트가 랜더링된 초기에만 실행된다.
-  useEffect(() => {
-    console.log("I run only once");
-  }, []);
-
-  // keyword라는 state가 변경될때만 실행된다.
-  useEffect(() => {
-    console.log("Keyword ", keyword, " find");
-  }, [keyword]);
-
-  // number라는 state나, keyword라는 state가 변경될 경우에만 실행된다.
-  useEffect(() => {
-    console.log(`Ketword : ${keyword} / number : ${number}`);
-  }, [keyword, number]);
-
-  const onClick = () => {
-    setNumber((number) => number + 1);
+      // React가 컴포넌트의 마운트가 해제되는 때에 clean-up을 실행시킨다.
+      // 각각의 effect는 특정한 랜더링에 속하게 된다. 그러므로 다음 차례의 랜더링을 실행하기 전에 이전의 랜더링에서 파생된 effect를 정리하기 위해 useEffect가 return 하는 clean-up 함수를 사용할 수 있다.
+      return () => {
+        console.log("I'm Out! Destroyed!");
+      };
+    }, []);
+    return <h1>Hello</h1>;
   };
 
-  const onChange = (event) => {
-    setKeyword((keyword) => event.target.value);
-  };
-
+  // 컴포넌트가 사라지면, 컴포넌트는 destroy되어 DOM에서도 실제로 사라진다.
   return (
-    <div>
-      <input type="text" placeholder="Search here" onChange={onChange} />
-      <span>{number}</span>
-      <button onClick={onClick}>Click me</button>
+    <div
+      onClick={() => {
+        setShowing((prev) => !prev);
+      }}
+    >
+      <button>{!showing ? "show it" : "hide it"}</button>
+      {showing ? <Hello /> : null}
     </div>
   );
 }
