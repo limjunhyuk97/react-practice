3e8e0a387ed57e5031bf51219cdb6142cb61cd53
limjunhyuk97 <limjunhyuk97@gmail.com>
2023-02-24 10:52:47 +0900

    [ReactJS로 영화 웹 서비스 만 들기] 코인 시세 api 활용 + 구매가능한 코인 수 계산 구현 : useEffect 사용하여 api data fetching, useEffect는 promise 객체 반환하면 안되므로 (clean-up 함수만 반환) async 함수를 첫번째 인자로 전달 불가. 함수 내부에서 실행해야 함. + 데이터 fetch 될때까지 state 값 false인 것 사용하여 fetch시 기다림 구현(스피너 구현 로직에 사용가능할 듯)

--- Nomad01/demo01/src/App.js
+++ Nomad01/demo01/src/App.js
@@ -3,39 +3,60 @@ import styles from "./App.module.css";
 import { useState, useEffect } from "react";
 
 function App() {
-  const [todo, setTodo] = useState("");
-  const [todos, setTodos] = useState([]);
+  const [loaded, setLoaded] = useState(false);
+  const [coins, setCoins] = useState([]);
+  const [money, setMoney] = useState(0);
+  const [selectedCoin, setSelectedCoin] = useState(0);
 
-  const onChange = (event) => {
-    setTodo((prev) => event.target.value);
-  };
+  function handleMoneyChange(event) {
+    setMoney((prev) => Number(event.target.value));
+  }
 
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (todo === "") return;
-    setTodos((prev) => [...prev, todo]);
-    setTodo("");
-  };
+  function handleCoinSelection(event) {
+    setSelectedCoin((prev) => event.target.value);
+  }
+
+  useEffect(() => {
+    async function fetchCoinData() {
+      await fetch("https://api.coinpaprika.com/v1/tickers")
+        .then((response) => response.json())
+        .then((data) => {
+          setCoins(data);
+          setLoaded(true);
+          console.log(data);
+        });
+    }
+    fetchCoinData();
+  }, []);
 
   return (
     <div>
-      <h1>My Todos ({todos.length})</h1>
-      <form onSubmit={onSubmit}>
+      <h1>The Coins! ({coins.length})</h1>
+      <h2>My money</h2>
+      <div style={{ display: "flex", gap: "16px", alignItems: "center" }}>
+        <input type="text" value={money} onChange={handleMoneyChange} />
+        <span>TO</span>
         <input
           type="text"
-          placeholder="Write to do"
-          onChange={onChange}
-          value={todo}
+          value={loaded ? money / coins[selectedCoin].quotes.USD.price : 0}
+          disabled
         />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {" "}
-        {todos.map((el, idx) => (
-          <li key={idx}>{el}</li>
-        ))}
-      </ul>
+        <span>Coins</span>
+      </div>
+      <h2>Coin options</h2>
+      {loaded ? (
+        <select onChange={handleCoinSelection}>
+          {coins.map((coin, idx) => (
+            <option key={coin.id} value={idx}>
+              <span> {coin.name}</span>
+              <span>({coin.symbol})</span>
+              <span> {coin.quotes.USD.price}</span>
+            </option>
+          ))}
+        </select>
+      ) : (
+        <div>Loading...</div>
+      )}
     </div>
   );
 }
