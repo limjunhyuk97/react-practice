1f5e8e56d5a3455a5dcb35140745413e9427555d
limjunhyuk97 <limjunhyuk97@gmail.com>
2025-08-10 17:49:15 +0900

    [RDG_3] 클래스형 컴포넌트 state 초기화 예시

--- rdg/rdg-2/src/RDG3_EX2.js
+++ rdg/rdg-2/src/RDG3_EX2.js
@@ -25,4 +25,5 @@ export default class App extends Component {
 /*
 * 클래스형 컴포넌트로 state 받아오기
   - constructor 내 super 실행 -> class 내부 영역에 정의된 state 필드 평가 -> constructor 내에 정의된 state 평가 
+  - 위와 같이 정의할 경우 this.state.num = 1 값으로 초기화 됨
 */
